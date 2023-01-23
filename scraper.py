e0760af4d9efc423d076ce89589b424d71b045eb
Max <43404248+impaex@users.noreply.github.com>
2023-01-23 23:35:58 +0100

    Support for multiple stores

--- scraper.py
+++ scraper.py
@@ -1,13 +1,23 @@
 import requests
 from stores.intertoys import get_intertoys_store_stock
+from stores.toychamp import get_toychamp_store_stock
 
 # Part to look for, also called Artikel Nummer
 partNumberIntertoys = 1990609
+# Also called bestelcode
+partNumberToychamp = '01662672'
 
-stock = get_intertoys_store_stock(partNumberIntertoys)
+toychamp_stock = get_toychamp_store_stock(partNumberToychamp)
+intertoys_stock = get_intertoys_store_stock(partNumberIntertoys)
 
-if stock:
-    for item in stock:
-        print(f"{item['storeNumber']} - {item['stock']}")
+if intertoys_stock:
+    for item in intertoys_stock:
+        print(f"{item['store']} - {item['stock']}")
 else:
     print('Something went wrong retrieving Intertoys store stock.')
+
+if toychamp_stock:
+    for item in toychamp_stock:
+        print(f"{item['store']} - {item['stock']}")
+else:
+    print('Something went wrong retrieving Toychamp store stock.')
